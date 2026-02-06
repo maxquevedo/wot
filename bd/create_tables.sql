1e6cb4c26961d4a8c1032bf33e6679a55aca74e9
Maximiliano Quevedo <maxquevedo@gmail.com>
2026-02-06 00:11:16 -0300

    chore: actualizacion bd

--- bd/create_tables.sql
+++ bd/create_tables.sql
@@ -1,42 +1,43 @@
 create table timezones(
-    id UUID primary key,
+    id bigint primary key,
     value varchar(3) not null
 );
 
 create table seasons(
-    id UUID primary key,
+    id bigint primary key,
     name varchar(6) not null,
     position int not null
 );
 
 create table continents(
-    id UUID primary key,
+    id bigint primary key,
     name varchar(60) not null,
-    current_season UUID references seasons(id) not null
+    current_season bigint references seasons(id) not null
 );
 
 create table countries(
-    id UUID primary key,
+    id bigint primary key,
     name varchar(60) not null,
+    code varchar(3) not null,
     timezone varchar(3) not null,
-    continent_id UUID references continents(id) not null,
-    season_id UUID references seasons(id) not null
+    continent_id bigint references continents(id) not null,
+    season_id bigint references seasons(id) not null
 );
 
 create table cities(
-    id UUID primary key,
+    id bigint primary key,
     name varchar(255) not null,
-    country_id UUID references countries(id) not null
+    country_id bigint references countries(id) not null
 );
 
 
 create table queries(
-    id UUID primary key,
+    id bigint primary key,
     query varchar(255) not null,
-    country_id UUID references countries(id) not null,
-    city_id UUID references cities(id) not null,
+    country_id bigint references countries(id) not null,
+    city_id bigint references cities(id) not null,
     gps_data text not null,
     response_data text not null,
-    created_at timestamp not null,
-    updated_at timestamp not null
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default current_timestamp()
 );
